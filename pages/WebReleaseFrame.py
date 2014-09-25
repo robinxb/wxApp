82f8acfdb632f928c7a8a70d7d04641edf6fdd72
robin <robinxiaobin@gmail.com>
2014-09-25 23:48:02 +0800

    update

--- pages/WebReleaseFrame.py
+++ pages/WebReleaseFrame.py
@@ -6,11 +6,90 @@ import utils
 import os
 import requests
 import wx
+import pysftp
+import paramiko
+import json
 
 ProductsURL = "http://pm.ejoy.com/HD.html"
 GetProductsURL = "http://pm.ejoy.com/admin/get_product"
 PostURL = "http://pm2.ejoy.com/admin/update_branch"
 GetBranchDetailURL = "http://pm.ejoy.com/admin/get_branch"
+
+def GetBranchInfo(sshobj, uuid):
+	sshin, sshout, ssherr = sshobj.exec_command('cd ~/project/branch && python cfg.py -l %s'%uuid)
+	success = False
+	js_str = ""
+	while True:
+		line = sshout.readline()
+		if line != "":
+			out = line.rstrip('\n')
+			js_str = js_str + out
+			if "404" in out:
+				break
+		else:
+			success = True
+			break
+	if success:
+		return success, json.loads(js_str)
+	else:
+		return success, js_str
+
+def CreateBranch(sshobj, data):
+	print '===== Create cfg on builder ====='
+	sshin, sshout, ssherr = sshobj.exec_command('cd ~/project/branch && python cfg.py -c %s --art %s --code %s'%(data["name"], data["artBranch"], data["codeBranch"]))
+	output = sshout.read().strip() + ssherr.read().strip()
+	if "Fatal" in output:
+		print output
+		return False
+
+	info = json.loads(output)
+
+	print '===== Ask builder to create branch on pm.ejoy.com ====='
+	cmd = "cd ~/project/branch && python upload.py create %s %s %s %s %s %s %s"%(data["name"], data["version"], data["desc"], data["package_type"], data["app_name"], data["bundle_id"], info["uuid"])
+	sshin, sshout, ssherr = sshobj.exec_command(cmd)
+	output = sshout.read().strip() + ssherr.read().strip()
+	print(output)
+	if not "CREATE_SUCCESS" in output:
+		return False
+
+	return True
+
+def BuildIPA(sshobj, codeBranch, artBranch):
+	print '===== Build IPA ====='
+	(sshin1, sshout1, ssherr) = sshobj.exec_command('cd ~/project && ./build.sh %s %s %s %s'%(codeBranch, artBranch))
+	success = False
+	while True:
+		line = sshout1.readline()
+		if line != '':
+			out = line.rstrip('\n')
+			print(out)
+			if "BUILDER_IPA_SUCCESS" in out:
+				success = True
+		else:
+			break
+	if not success:
+		print '===== Build Fail, Please Check The Error ====='
+		return False
+	return True
+
+def UploadIPA(sshobj, uuid, pathOnRemote):
+	print '===== Update IPA to pm.ejoy.com ====='
+
+	(sshin1, sshout1, ssherr) = sshobj.exec_command('cd ~/project/branch && python upload.py update %s %s '%(uuid, pathOnRemote))
+	success = False
+	while True:
+		line = sshout1.readline()
+		if line != '':
+			out = line.rstrip('\n')
+			print(out)
+			if "UPLOAD_SUCCESS" in out:
+				success = True
+		else:
+			break
+	if not success:
+		print '===== Upload IPA Fail, Please Check The Error ====='
+	return success
+
 
 class branchDetail(object):
 	def __init__(self):
@@ -32,14 +111,17 @@ class branchDetail(object):
 		self.version2 = None
 		self.version3 = None
 
-	def FeedWithHtml(self, rawhtml):
-		pass
-
 class WebReleaseFrame(_extend.WebReleaseFrame):
 	def __init__(self, parent):
 		super(WebReleaseFrame, self).__init__(parent)
+		ip = utils.path.GetBuilderIP()
+		self.ssh = paramiko.SSHClient()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		conn = self.ssh.connect(ip, username = 'farmbuilder', password = "farm")
+		print '===== SSH connected to %s ====='%(ip)
 		self._fetchAll()
 		self._loadAll()
+
 
 	def _fetchAll(self):
 		self.branches = []
@@ -54,14 +136,19 @@ class WebReleaseFrame(_extend.WebReleaseFrame):
 			bObject.update_time = j[u'update_time']
 			bObject.create_time = j[u'create_time']
 			bObject.branchid = j[u'id']
-			bObject.version_for_web = j[u'version']
-			bObject.desc = j[u'desc']
+			bObject.uuid= j[u'version'] #暂时用来存uuid
+			bObject.desc= j[u'desc']
 			ptype = j[u'package_type']
 			bObject.type = ptype
 			if ptype == 'ios':
 				bObject.bundle_id = j[u'spec_items'][u'bundle_id']
 				bObject.app_name = j[u'spec_items'][u'app_name']
-			self.branches.append(bObject)
+
+			if bObject.uuid:
+				success, info = GetBranchInfo(self.ssh, bObject.uuid)
+				print(success, info)
+				if success:
+					self.branches.append(bObject)
 
 	def _loadAll(self):
 		# self.m_ItemType.InsertChoice("ios", 1)
@@ -105,6 +192,51 @@ class WebReleaseFrame(_extend.WebReleaseFrame):
 			self.m_ItemAppName.SetValue(obj.app_name)
 			self.m_ItemBundleID.SetValue(obj.bundle_id)
 
-		# self.m_ItemType.EditEnumProperty("EditEnum", "EditEnumProperty", ['A', 'B', 'C'], [0, 1, 2], "Text Not in List")
-		# self.m_ItemType.SetChoices(obj.type)
-		# print self.m_ItemType.GetChoices()
+	def _OnClickExecute( self, event):
+		event.Skip()
+
+
+		print '===== Make ipd for CodeBranch: %s and ArtBranch: %s'%(codeBranch, artBranch)
+
+		selectId = self.m_BranchList.GetSelection()
+		if selectId == wx.NOT_FOUND:
+			return
+		obj = self.branches[selectId]
+		branch_name = obj.name
+		success, info = GetBranchInfo(self.ssh, branch_name)
+
+		if not success:
+			print '===== Branch %s NOT FOUND on remote, try to create one ====='%(branch_name)
+
+	def _OnClickAddButton( self, event):
+		# "branch_name": data.name,
+		# "version": data.version,
+		# "desc": data.desc,
+		# "package_type": data.package_type,
+		# "app_name": data.app_name,
+		# "bundle_id": data.bundle_id
+		# data.artBranch
+		# data.codeBranch
+		event.Skip()
+		bIsIOS = self.m_ItemIsIos.GetValue() == True
+		artBranch, codeBranch = self.m_ArtBranch.GetStringSelection(), self.m_CodeBranch.GetStringSelection()
+		artBranch = artBranch.split("/")[-1]
+		codeBranch = codeBranch.split("/")[-1]
+		data = {
+		"name": self.m_ItemBranchName.GetValue(),
+		"version": ".".join([
+			str(self.m_ItemVersion1.GetValue()),
+			str(self.m_ItemVersion2.GetValue()),
+			str(self.m_ItemVersion3.GetValue())
+		]),
+		"desc": self.m_ItemDesc.GetValue(),
+		"package_type":  bIsIOS and "ios" or "android",
+		"artBranch": artBranch,
+		"codeBranch": codeBranch,
+		}
+		if bIsIOS:
+			data["bundle_id"] = self.m_ItemBundleID.GetValue()
+			data["app_name"] = self.m_ItemAppName.GetValue()
+
+		if not CreateBranch(self.ssh, data):
+			return
